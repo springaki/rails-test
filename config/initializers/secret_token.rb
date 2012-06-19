37a3da2941096ab9904f8da124d11988ca57257c
Ubuntu <ubuntu@ip-10-148-51-196.ap-northeast-1.compute.internal>
2012-06-19 04:08:08 +0000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsTest::Application.config.secret_token = 'e793e48d49c1f9e6745c9847eabc3504fb651834a880e71f41e17197a030bb3aa42cfb6c2fffeaf7d6b31159dd9fc6023895875a46b1e05fd94f016e13d7390c'
